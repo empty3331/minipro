639ef58fd1d9eb4b1dca226b121cd2655552e57c
'Ju-Hye' <'empty3331@naver.com'>
2020-06-03 11:48:05 +0900

    3번 푸는중

--- src/com/javaex/ex03/PhoneRepository.java
+++ src/com/javaex/ex03/PhoneRepository.java
@@ -1,31 +1,70 @@
 package com.javaex.ex03;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
 import java.util.List;
+
 
 public class PhoneRepository {
 
-    
-    //phoneDB.txt 파일을 읽어 모든 전화번호(리스트)를 전달하는 메소드
-	public List<Person> getList(){
-		
+	private List<Person> pList = new ArrayList<Person>();
+
+	// phoneDB.txt 파일을 읽어 모든 전화번호(리스트)를 전달하는 메소드
+	public List<Person> getList() throws IOException {
+		InputStream in = new FileInputStream("C:\\javaStudy\\workspace\\minipro\\PhoneDB.txt");
+		InputStreamReader isr = new InputStreamReader(in, "UTF-8");
+		BufferedReader br = new BufferedReader(isr);
+
+		List<Person> pList = new ArrayList<Person>();
+
+		while (true) {
+			String str = br.readLine();
+			if (str == null) {
+				break;
+			}
+
+			String[] splitInfo = str.split(",");
+
+			Person person = new Person(splitInfo[0], splitInfo[1], splitInfo[2]);
+			pList.add(person);
+			pList = getList();
+		}
+		br.close();
+		return pList;
+	}
+
+	// phoneDB.txt 에 모든 전화번호 리스트를 저장하는 메소드
+	public void saveInfo(Person phoneVO) throws IOException {
+		OutputStream out = new FileOutputStream("C:\\javaStudy\\workspace\\minipro\\PhoneDB.txt");
+		OutputStreamWriter ow = new OutputStreamWriter(out, "UTF-8");
+		BufferedWriter bw = new BufferedWriter(ow);
+
+		for (Person p1 : pList) {
+			bw.write(p1.getName() + "," + p1.getHp() + "," + p1.getCompany());
+			bw.newLine();
+		}
+
+		bw.close();
+	}
+
+	// 기존데이터에 새로입력받은 데이터를 추가하여 모두저장하는 메소드
+	public void addInfo(Person phoneVO) throws IOException {
 		
 	}
 
-	//phoneDB.txt 에 모든 전화번호 리스트를 저장하는 메소드
-	private void saveInfo(List<Person> list){
-		 
+	// 선택한 번호의 데이터를 삭제하고 저장하는 메소드(모두 다시저장)
+	public void delInfo(int num) throws IOException {
 		
-	}
 	
-	//기존데이터에 새로입력받은 데이터를 추가하여 모두저장하는 메소드 
-	public void addInfo(Person phoneVO) {
-		
-	}
 
-	//선택한 번호의 데이터를 삭제하고 저장하는 메소드(모두 다시저장)
-	public void delInfo(int num) {
-		
 	}
-	
 
 }
--- src/com/javaex/ex03/PhoneView.java
+++ src/com/javaex/ex03/PhoneView.java
@@ -3,6 +3,8 @@ package com.javaex.ex03;
 import java.util.List;
 import java.util.Scanner;
 
+import com.javaex.ex02.Person;
+
 //화면과 관련된 기능을 담당합니다.
 
 public class PhoneView {
@@ -16,21 +18,41 @@ public class PhoneView {
 
 	// 프로그램 시작시 안내 문구를 출력하는 메소드
 	public void showStart() {
-
+		System.out.println("******************************************");
+		System.out.println("*         전화번호 관리 프로그램         *");
+		System.out.println("******************************************");
 	}
 
 	// 메뉴를 출력하고 메뉴번호 입력을 받아 입력된 메뉴번호를 전달하는 메소드
 	public int showMenu() {
-
+			System.out.println("                                          ");
+			System.out.println("1.리스트 2.등록 3.삭제 4.검색 5.종료");
+			System.out.println("---------------------------------------");
+			System.out.print(">메뉴번호:");
+			int i = sc.nextInt();
+			return i;
 	}
 
 	// 1.리스트 : 데이터를 받아 리스트를 출력하는 메소드
 	public void showList(List<Person> phoneList) {
+		System.out.println("<1.리스트>");
 
 	}
 
 	// 2.등록 : 등록을 위한 화면을 출력하고 사용자가 입력한 데이트를 받아 Person의 인스턴스에 담아 전달하는 메소드
 	public Person showAdd() {
+		System.out.println("<2.등록>");
+		System.out.print(">이름:");
+		String name = sc.next();
+		System.out.print(">휴대전화");
+		String hp = sc.next();
+		System.out.print(">회사전화");
+		String company = sc.next();
+
+		// 리스트
+		Person pp = new Person(name, hp, company);
+		return pp;
+		
 
 	}
 
@@ -56,17 +78,25 @@ public class PhoneView {
 
 	// 검색결과를 가져와 화면에 출력하는 메소드
 	public void showSearchResult(List<Person> phoneList, String keyword) {
+		System.out.println("<4.검색>");
+		  System.out.print(">이름:");
+		  String sh = sc.next();
 
 	}
 
 	// 메뉴번호를 잘못 입력시 안내문구를 출력하는 메소드
 	public void showEtc() {
+		System.out.println("[다시 입력해주세요.]");
 
 	}
 	
 	
 	// 종료시 안내 문구를 출력하는 메소드
 	public void showEnd() {
+		System.out.println("******************************************");
+		System.out.println("*                감사합니다              *");
+		System.out.println("******************************************");
+
 
 	}
 }
